a757aef49ca899e9deb8c1bb0295ad91f6f4feca
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-11-26 12:50:32 +0300

    added an error handling file

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
+const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
 
 const app = express();
-app.use(express.json)
-
-app.use("/api/contacts", require("./routes/contactsRoutes"));
 
 const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use("/api/contacts", require("./routes/contactsRoutes"));
+app.use(errorHandler);
+
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
