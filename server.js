b7018b25d4659ffafb85a210ba8bfb9e21f834c6
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-12-11 10:13:15 +0300

    added controllers to the project

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const dotenv = require("dotenv").config();
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = 5001;
+
+app.use("/api/contacts", require("./routes/contactRoutes"))
 
 app.listen(port , () => {
   console.log(`Server running on port ${port}`)
